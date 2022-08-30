47b70f7cc881166e18b8aaf567466fbd357444d1
DESKTOP-5FESLB9\envyst <nurardli.saputra@gmail.com>
2022-08-30 10:34:00 +0700

    add examples

---
+++ examples/test-sd-lora-esp32/test-sd-lora-esp32.ino
@@ -0,0 +1,342 @@
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+#include <lorawan.h> //currently using HSPI esp32
+
+bool written{false};
+
+#define sdCS 5
+#define loraCS 15
+
+//ABP Credentials
+const char *devAddr = "db48c21b";
+const char *nwkSKey = "93df5e92cbaf1efc0000000000000000";
+const char *appSKey = "000000000000000002e21425cc8ccfa2";
+
+const unsigned long interval = 10000;    // 10 s interval to send message
+unsigned long previousMillis = 0;  // will store last time message sent
+unsigned int counter = 0;     // message counter
+
+char myStr[50];
+byte outStr[255];
+byte recvStatus = 0;
+int port, channel, freq;
+bool newmessage = false;
+
+const sRFM_pins RFM_pins = {
+  .loraHSPI = true;
+  .CS = 15,
+  .RST = 2,
+  .DIO0 = 33,
+  .DIO1 = 34,
+};
+
+//sd card procedure
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.name(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void createDir(fs::FS &fs, const char * path){
+    Serial.printf("Creating Dir: %s\n", path);
+    if(fs.mkdir(path)){
+        Serial.println("Dir created");
+    } else {
+        Serial.println("mkdir failed");
+    }
+}
+
+void removeDir(fs::FS &fs, const char * path){
+    Serial.printf("Removing Dir: %s\n", path);
+    if(fs.rmdir(path)){
+        Serial.println("Dir removed");
+    } else {
+        Serial.println("rmdir failed");
+    }
+}
+
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if(!file){
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+    file.close();
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+    file.close();
+}
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+    Serial.printf("Renaming file %s to %s\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("File renamed");
+    } else {
+        Serial.println("Rename failed");
+    }
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\n", path);
+    if(fs.remove(path)){
+        Serial.println("File deleted");
+    } else {
+        Serial.println("Delete failed");
+    }
+}
+
+void testFileIO(fs::FS &fs, const char * path){
+    File file = fs.open(path);
+    static uint8_t buf[512];
+    size_t len = 0;
+    uint32_t start = millis();
+    uint32_t end = start;
+    if(file){
+        len = file.size();
+        size_t flen = len;
+        start = millis();
+        while(len){
+            size_t toRead = len;
+            if(toRead > 512){
+                toRead = 512;
+            }
+            file.read(buf, toRead);
+            len -= toRead;
+        }
+        end = millis() - start;
+        Serial.printf("%u bytes read for %u ms\n", flen, end);
+        file.close();
+    } else {
+        Serial.println("Failed to open file for reading");
+    }
+
+
+    file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+
+    size_t i;
+    start = millis();
+    for(i=0; i<2048; i++){
+        file.write(buf, 512);
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+    file.close();
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {}  // wait for Leonardo
+  if(!SD.begin(sdCS)){
+        Serial.println("Card Mount Failed");
+        return;
+    }
+    uint8_t cardType = SD.cardType();
+
+    if(cardType == CARD_NONE){
+        Serial.println("No SD card attached");
+        return;
+    }
+
+    Serial.print("SD Card Type: ");
+    if(cardType == CARD_MMC){
+        Serial.println("MMC");
+    } else if(cardType == CARD_SD){
+        Serial.println("SDSC");
+    } else if(cardType == CARD_SDHC){
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
+
+    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+    Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
+    listDir(SD, "/", 0);
+    writeFile(SD, "/hello.txt", "Hello ");
+    appendFile(SD, "/hello.txt", "World!\n");
+    readFile(SD, "/hello.txt");
+    deleteFile(SD, "/foo.txt");
+    renameFile(SD, "/hello.txt", "/foo.txt");
+    readFile(SD, "/foo.txt");
+    testFileIO(SD, "/test.txt");
+    Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+    Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+
+  while (!lora.init()) {
+    Serial.println("LoRa radio init failed");
+    delay(1000);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Set LoRaWAN Class change CLASS_A or CLASS_C
+  lora.setDeviceClass(CLASS_C);
+
+  // Set Data Rate
+  lora.setDataRate(SF9BW125);
+
+  // Set FramePort Tx
+  lora.setFramePortTx(5);
+
+  // set channel to random
+  lora.setChannel(MULTI);
+
+  // Set TxPower to 15 dBi (max)
+  lora.setTxPower(15);
+
+  // Put ABP Key and DevAddress here
+  lora.setNwkSKey(nwkSKey);
+  lora.setAppSKey(appSKey);
+  lora.setDevAddr(devAddr);
+}
+
+void loop()
+{  
+  // Check interval overflow
+  if (millis() - previousMillis > interval) {
+    previousMillis = millis();
+
+    sprintf(myStr, "Lora Counter-%d", counter++);
+    
+    Serial.print("Sending: ");
+    Serial.println(myStr);
+    lora.sendUplink(myStr, strlen(myStr), 0);
+    port = lora.getFramePortTx();
+    channel = lora.getChannel();
+    freq = lora.getChannelFreq(channel);
+    Serial.print(F("fport: "));    Serial.print(port);Serial.print(" ");
+    Serial.print(F("Ch: "));    Serial.print(channel);Serial.print(" ");
+    Serial.print(F("Freq: "));    Serial.print(freq);Serial.println(" ");
+    delay(1000);
+    
+    if(!written) 
+    {
+      writeFile(SD, "/testing.txt", "Test File 1 2 3 ...");
+      written = true;
+    }
+    else 
+    {
+      written = false;
+      readFile(SD, "/testing.txt");
+    }
+  }
+
+  // Check Lora RX
+  lora.update();
+
+  recvStatus = lora.readDataByte(outStr);
+  if (recvStatus) {
+    newmessage = true;
+    int counter = 0;
+    port = lora.getFramePortRx();
+    channel = lora.getChannelRx();
+    freq = lora.getChannelRxFreq(channel);
+
+    for (int i = 0; i < recvStatus; i++)
+    {
+      if (((outStr[i] >= 32) && (outStr[i] <= 126)) || (outStr[i] == 10) || (outStr[i] == 13))
+        counter++;
+    }
+    if (port != 0)
+    {
+      if (counter == recvStatus)
+      {
+        Serial.print(F("Received String : "));
+        for (int i = 0; i < recvStatus; i++)
+        {
+          Serial.print(char(outStr[i]));
+        }
+      }
+      else
+      {
+        Serial.print(F("Received Hex: "));
+        for (int i = 0; i < recvStatus; i++)
+        {
+          Serial.print(outStr[i], HEX); Serial.print(" ");
+        }
+      }
+      Serial.println();
+      Serial.print(F("fport: "));    Serial.print(port);Serial.print(" ");
+      Serial.print(F("Ch: "));    Serial.print(channel);Serial.print(" ");
+      Serial.print(F("Freq: "));    Serial.println(freq);Serial.println(" ");
+    }
+    else
+    {
+      Serial.print(F("Received Mac Cmd : "));
+      for (int i = 0; i < recvStatus; i++)
+      {
+        Serial.print(outStr[i], HEX); Serial.print(" ");
+      }
+      Serial.println();
+      Serial.print(F("fport: "));    Serial.print(port);Serial.print(" ");
+      Serial.print(F("Ch: "));    Serial.print(channel);Serial.print(" ");
+      Serial.print(F("Freq: "));    Serial.println(freq);Serial.println(" ");
+    }
+  }
+}
